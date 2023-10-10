6063a0b44392191800c1a01f4141cf418a4b4117
Mateus Caetano <mateuscaetano952@gmail.com>
2023-10-10 12:51:14 -0300

    avanço
    Adicionado a movimentação do cactu

--- scrip.js
+++ scrip.js
@@ -1,9 +1,21 @@
 let dino = document.getElementById('dino');
 
 function pular(){
+    if(dino.classList.contains('pular')) return;
+
     dino.classList.add('pular')
+    setTimeout(() => dino.classList.remove('pular'), 1000)
+
 }
 
-document.addEventListener(e){
-    
-}
+document.addEventListener('keydown', function(event) {
+    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+
+    switch (event.key) {
+        case "ArrowUp":
+            pular()
+            break;
+
+    }
+});
+
