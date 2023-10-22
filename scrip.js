65cd34a45aa9a4ffb38a11e2c5868257ec9a03d1
Mateus Caetano <mateuscaetano952@gmail.com>
2023-10-22 20:40:00 -0300

    1

--- scrip.js
+++ scrip.js
@@ -4,13 +4,13 @@ function pular(){
     if(dino.classList.contains('pular')) return;
 
     dino.classList.add('pular')
-    setTimeout(() => dino.classList.remove('pular'), 1000)
+    setTimeout(() => dino.classList.remove('pular'), 500)
 
 }
 
 document.addEventListener('keydown', function(event) {
-    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
-
+    const key = event.key;
+    
     switch (event.key) {
         case "ArrowUp":
             pular()
